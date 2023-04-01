0bf9ea5fbed0c05e17f39cbea8693a25580c55a0
Brett S <69087616+bslate90@users.noreply.github.com>
2023-04-01 14:40:41 -0500

    Update script.js
    squashed bugs within my own code - I'm learning!

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	  	name:"Repost influencer Posts"
 	  	date: "April 21"
 	  	description: "April 21st-25th: Repost all of the Incfluencer's Posts and encourage our audience to check their social accounts out."
-	  }
+	  },
 	  {
 	    name: "Announce the Expo Countdown Calendar with Special event's",
 	    date: "April 25",
